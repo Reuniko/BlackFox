74775ccec2907043aa4c8ef7f8d452c239ded540
Reuniko Starwinged <319765+Reuniko@users.noreply.github.com>
2017-09-28 14:20:54 +0300

    wip Event element

--- modules/System/classes/Utility.php
+++ modules/System/classes/Utility.php
@@ -2,5 +2,21 @@
 namespace System;
 
 class Utility extends Instanceable {
-
+	/**
+	 * Форматирует дату в любом формате (стока|таймштамп)
+	 * Возвращает пустую строку если дата пуста
+	 *
+	 * @param string|int $date дата в любом формате
+	 * @param string $format формат (по умолчанию 'd.m.Y H:i')
+	 * @return string отформатированная дата
+	 */
+	public static function Date($date, $format = 'd.m.Y H:i') {
+		if (empty($date)) {
+			return '';
+		}
+		if (!is_numeric($date)) {
+			$date = strtotime($date);
+		}
+		return date($format, $date);
+	}
 }
