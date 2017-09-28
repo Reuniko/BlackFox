704aa5242fde6d69e6d48ae56795591c07890122
Reuniko Starwinged <319765+Reuniko@users.noreply.github.com>
2017-09-28 13:50:34 +0300

    wip Event element

--- modules/System/classes/html/HtmlImage.php
+++ modules/System/classes/html/HtmlImage.php
@@ -4,10 +4,28 @@ namespace System;
 
 class HtmlImage {
 
-	public static function Proportional($src, $height = 1) {
+	public static function Proportional($src, $proportion = 1) {
 		?>
 		<div style="position: relative; width: 100%;">
-			<div style="display: block; padding-top: <?= ceil($height * 100) ?>%; "></div>
+			<div style="display: block; padding-top: <?= ceil($proportion * 100) ?>%; "></div>
+			<div style="
+				position:  absolute;
+				top: 0;
+				left: 0;
+				bottom: 0;
+				right: 0;
+				background-image: url('<?= $src ?>');
+				background-size: cover;
+				background-position: center center;
+				"></div>
+		</div>
+		<?
+	}
+
+	public static function FixedHeight($src, $height) {
+		?>
+		<div style="position: relative; width: 100%;">
+			<div style="display: block; padding-top: <?= $height ?>; "></div>
 			<div style="
 				position:  absolute;
 				top: 0;
