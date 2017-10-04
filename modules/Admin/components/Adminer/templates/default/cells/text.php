4d01030e9b0fd9c4121281682bf79a1d61c582fb
Reuniko Starwinged <319765+Reuniko@users.noreply.github.com>
2017-10-04 12:35:22 +0300

    wip adminer section list (micro)

--- modules/Admin/components/Adminer/templates/default/cells/text.php
+++ modules/Admin/components/Adminer/templates/default/cells/text.php
@@ -1,10 +1,8 @@
 <? /** @var string $value */ ?>
-<div style="min-width: 300px">
-	<?
-	$value = strip_tags($value);
-	if (mb_strlen($value) > 100) {
-		$value = mb_substr($value, 0, 100) . '...';
-	}
-	echo $value;
-	?>
-</div>
+<?
+$value = strip_tags($value);
+if (mb_strlen($value) > 100) {
+	$value = mb_substr($value, 0, 100) . '...';
+}
+echo $value;
+?>
