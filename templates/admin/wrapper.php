cb75f22200c4ae5d57b6228d9d98b37d497b1e8c
Reuniko <reuniko@gmail.com>
2017-10-05 16:02:10 +0300

    better file handling

--- templates/admin/wrapper.php
+++ templates/admin/wrapper.php
@@ -16,7 +16,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="/admin/">Admin</a>
+			<a class="navbar-brand" href="/">Site</a>
 		</div>
 		<div id="navbar" class="navbar-collapse collapse">
 			<ul class="nav navbar-nav navbar-right">
