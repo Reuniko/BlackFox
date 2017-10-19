467b46f547c1e27f8f192cd1f6bb9a3c1bced11a
Reuniko <reuniko@gmail.com>
2017-10-19 13:56:39 +0300

    Events

--- modules/System/classes/orm/types/TypeList.php
+++ modules/System/classes/orm/types/TypeList.php
@@ -8,12 +8,16 @@ class TypeList extends TypeText {
 
 	public function FormatInputValue($value, $info = []) {
 		$value = is_array($value) ? $value : [$value];
+		$value = array_filter($value, 'strlen');
 		$value = json_encode($value, JSON_UNESCAPED_UNICODE);
 		return parent::FormatInputValue($value, $info);
 	}
 
 	public function FormatOutputValue($element, $code, $info) {
 		$element[$code] = json_decode($element[$code], true);
+		if (json_last_error()) {
+			$element[$code] = [];
+		}
 		return parent::FormatOutputValue($element, $code, $info);
 	}
 
