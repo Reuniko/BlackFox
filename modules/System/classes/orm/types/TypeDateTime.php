60d2a4b171ff11c0aea70960f90828585ecc041a
Reuniko Starwinged <319765+Reuniko@users.noreply.github.com>
2017-11-02 12:34:53 +0300

    game apply

--- modules/System/classes/orm/types/TypeDateTime.php
+++ modules/System/classes/orm/types/TypeDateTime.php
@@ -2,20 +2,11 @@
 
 namespace System;
 
-class TypeDateTime extends Type {
+class TypeDateTime extends TypeDate {
 	public $name = 'DateTime';
 	public $code = 'DATETIME';
 
 	public function GetStructureStringType($info = []) {
 		return 'datetime';
-	}
-
-	public function FormatInputValue($value, $info = []) {
-		if (is_numeric($value)) {
-			$value = date(\DateTime::ISO8601, $value);
-		} else {
-			$value = date(\DateTime::ISO8601, strtotime($value));
-		}
-		return $value;
 	}
 }
