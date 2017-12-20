8ed25a116e7726aace9e665b3a201c44bb97f701
Reuniko Starwinged <319765+Reuniko@users.noreply.github.com>
2017-12-20 15:11:47 +0300

    TypeDateTime triggers

--- modules/System/classes/orm/types/TypeDateTime.php
+++ modules/System/classes/orm/types/TypeDateTime.php
@@ -9,4 +9,15 @@ class TypeDateTime extends TypeDate {
 	public function GetStructureStringType($info = []) {
 		return 'datetime';
 	}
+
+	public function GetStructureString($code, $info = []) {
+		$string = parent::GetStructureString($code, $info);
+		if ($info['TRIGGER'] === 'CREATE') {
+			$string = "{$string} DEFAULT CURRENT_TIMESTAMP";
+		}
+		if ($info['TRIGGER'] === 'UPDATE') {
+			$string = "{$string} ON UPDATE CURRENT_TIMESTAMP";
+		}
+		return $string;
+	}
 }
