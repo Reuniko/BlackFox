4f7a73f4928da5aaf39e887d2ba660d578680c45
Reuniko Starwinged <319765+Reuniko@users.noreply.github.com>
2018-02-08 12:51:32 +0300

    menu 2 levels

--- modules/Admin/components/Menu/templates/default/template.php
+++ modules/Admin/components/Menu/templates/default/template.php
@@ -1,29 +1,55 @@
 <?php /** @var \System\Component $this */ ?>
 <?php $this->Debug($RESULT, '$RESULT'); ?>
+<?
+$icons = [
+	'category' => 'fa fa-caret-right',
+	'item'     => 'fa fa-stop fa-vs',
+];
+?>
 
-<div class="menu list-group" data-menu="">
+<script><? include('script.js') ?></script>
+<style><? include('style.css') ?></style>
+
+<h2 class="text-center m-2">Меню</h2>
+
+<ul class="menu" data-menu="">
 	<? foreach ($RESULT as $id1 => $element1): ?>
-		<a
-			class="list-group-item level1 p-2 <?= $element1['ACTIVE'] ? 'active' : '' ?>"
-			href="<?= $element1['LINK'] ?: '#' ?>"
+		<li>
+			<a
+				class="level1 <?= $element1['ACTIVE'] ? 'active' : '' ?>"
+				href="<?= $element1['LINK'] ?: '#' ?>"
+				<? if ($element1['CHILDREN']): ?>
+					data-menu-category=""
+				<? endif; ?>
+			>
+				<span>
+					<? if ($element1['CHILDREN']): ?>
+						<i class="fa-level <?= $icons['category'] ?> <?= $element1['ACTIVE'] ? 'fa-rotate-90' : '' ?>" data-menu-rotator=""></i>
+					<? else: ?>
+						<i class="fa-level <?= $icons['item'] ?>"></i>
+					<? endif; ?>
+					<?= $element1['NAME'] ?>
+				</span>
+			</a>
 			<? if ($element1['CHILDREN']): ?>
-				data-menu-category=""
+				<ul data-menu-children="" class="<?= $element1['ACTIVE'] ? '' : 'collapse' ?>">
+					<? foreach ($element1['CHILDREN'] as $id2 => $element2): ?>
+						<li>
+							<a
+								class="level2 <?= $element2['ACTIVE'] ? 'active' : '' ?>"
+								href="<?= $element2['LINK'] ?>"
+							>
+								<span>
+									<i class="fa-level <?= $icons['item'] ?>"></i>
+									<?= $element2['NAME'] ?>
+								</span>
+							</a>
+						</li>
+					<? endforeach; ?>
+				</ul>
 			<? endif; ?>
-		>
-			<span><?= $element1['NAME'] ?></span>
-		</a>
-		<? if ($element1['CHILDREN']): ?>
-			<div data-menu-children="" class="<?= $element1['ACTIVE'] ? '' : 'collapse' ?>">
-				<? foreach ($element1['CHILDREN'] as $id2 => $element2): ?>
-					<a
-						class="list-group-item level2 p-1 pl-3 <?= $element2['ACTIVE'] ? 'active' : '' ?>"
-						href="<?= $element2['LINK'] ?>"
-					><span><?= $element2['NAME'] ?></span></a>
-				<? endforeach; ?>
-			</div>
-		<? endif; ?>
+		</li>
 	<? endforeach; ?>
-</div>
+</ul>
 
-<script><?include('script.js')?></script>
-<style><?include('style.css')?></style>
+
