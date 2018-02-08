a414f44547299ec1a55c562d56a040e611b44cd3
Reuniko <reuniko@gmail.com>
2018-02-08 12:51:32 +0300

    menu 2 levels

--- modules/Admin/components/Menu/templates/default/script.js
+++ modules/Admin/components/Menu/templates/default/script.js
@@ -1,5 +1,6 @@
 $(function () {
 	$('[data-menu-category]').click(function () {
 		$(this).siblings('[data-menu-children]').slideToggle();
+		$(this).find('[data-menu-rotator]').toggleClass('fa-rotate-90');
 	});
 });
