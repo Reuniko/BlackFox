544692de148e3e1c1c454616bee651192c99a932
Reuniko <reuniko@gmail.com>
2018-02-08 17:42:07 +0300

    menu infinite levels expander rotation

--- modules/Admin/components/Menu/templates/default/script.js
+++ modules/Admin/components/Menu/templates/default/script.js
@@ -1,6 +1,6 @@
 $(function () {
 	$('[data-menu-expander]').click(function () {
-		$(this).closest('[data-menu-item]').find('[data-menu-children]').toggle();
-		$(this).toggleClass('fa-rotate-90');
+		$(this).closest('[data-menu-item]').children('[data-menu-children]').toggle();
+		$(this).toggleClass('rotate-90').toggleClass('rotate-0');
 	});
 });
