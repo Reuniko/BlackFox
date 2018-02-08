db0ae985eeca1338c53f5739119302ad2bfcf211
Reuniko Starwinged <319765+Reuniko@users.noreply.github.com>
2018-02-08 17:45:34 +0300

    migrate TestRunner for bootstrap 4

--- modules/Admin/components/TestRunner/templates/default/template.php
+++ modules/Admin/components/TestRunner/templates/default/template.php
@@ -23,19 +23,19 @@
 	?>
 
 	<? foreach ($RESULT as $test_class_name => $test): ?>
-		<div class="panel panel-default">
-			<div class="panel-heading" title="<?= $test_class_name ?>">
+		<div class="card">
+			<div class="card-header" title="<?= $test_class_name ?>">
 				<?= $test['NAME'] ?>
 			</div>
 			<? if (!empty($test['RESULTS'])): ?>
-				<div class="panel-body">
+				<div class="card-block">
 					<? foreach ($test['RESULTS'] as $test_method_name => $test_method_result): ?>
 						<? // debug($test_method_result) ?>
 						<div class="alert alert-sm <?= $status_to_class[$test_method_result['STATUS']] ?>">
 							<strong><?= $test_method_result['NAME'] ?></strong>
 							<? if (!empty($test_method_result['ERROR'])): ?>
 								<hr/>
-								<?= print_r($test_method_result['RESULT']); ?>
+								<?= $test_method_result['ERROR']; ?>
 							<? endif; ?>
 							<? if (!empty($test_method_result['RESULT'])): ?>
 								<? debug($test_method_result['RESULT']); ?>
