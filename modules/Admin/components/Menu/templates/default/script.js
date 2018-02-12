5f7a87d5bfba65a741acf6ef5947aa37866538bb
Reuniko <reuniko@gmail.com>
2018-02-12 18:56:53 +0300

    Admin Menu fixes

--- modules/Admin/components/Menu/templates/default/script.js
+++ modules/Admin/components/Menu/templates/default/script.js
@@ -1,6 +1,6 @@
 $(function () {
-	$('[data-menu-expander]').click(function () {
-		$(this).closest('[data-menu-item]').children('[data-menu-children]').toggle();
-		$(this).toggleClass('rotate-90').toggleClass('rotate-0');
-	});
+    $('[data-menu-expander]').click(function () {
+        $(this).closest('[data-menu-item]').children('[data-menu-children]').toggle();
+        $(this).closest('[data-menu-item]').find('[data-menu-rotate]').toggleClass('rotate-90').toggleClass('rotate-0');
+    });
 });
--- modules/Admin/components/Menu/templates/default/template.php
+++ modules/Admin/components/Menu/templates/default/template.php
@@ -1,28 +1,33 @@
 <?php /** @var \System\Component $this */ ?>
 <?php $this->Debug($RESULT, '$RESULT'); ?>
-<script src="<?= $this->template_relative_folder ?>/script.js"></script>
-<link rel='stylesheet' href="<?= $this->template_relative_folder ?>/style.css"/>
+	<script src="<?= $this->template_relative_folder ?>/script.js"></script>
+	<link rel='stylesheet' href="<?= $this->template_relative_folder ?>/style.css"/>
 
-<h2 class="menu-title collapse">Меню</h2>
+	<h2 class="menu-title collapse">Меню</h2>
 
 <? function BuildDefaultMenuRecursive($item) { ?>
 	<li data-menu-item="">
 		<div class="item <?= $item['ACTIVE'] ? 'active' : '' ?> <?= $item['CURRENT'] ? 'current' : '' ?>">
 
 			<? if ($item['CHILDREN']): ?>
-				<i class="menu-point menu-point-category <?= $item['ACTIVE'] ? 'rotate-90' : 'rotate-0' ?>" data-menu-expander=""></i>
+				<i class="menu-point menu-point-category <?= $item['ACTIVE'] ? 'rotate-90' : 'rotate-0' ?>"
+					data-menu-expander=""
+					data-menu-rotate=""
+				></i>
 			<? else: ?>
 				<i class="menu-point menu-point-item"></i>
 			<? endif; ?>
 
-			<a
-				href="<?= $item['LINK'] ?: '#' ?>"
-				<? if ($item['CHILDREN']): ?>
-					data-menu-category=""
-				<? endif; ?>
-			>
-				<?= $item['NAME'] ?>
-			</a>
+			<? if ($item['LINK']): ?>
+				<a href="<?= $item['LINK'] ?: '#' ?>">
+					<?= $item['NAME'] ?>
+				</a>
+			<? else: ?>
+				<span data-menu-expander="">
+					<?= $item['NAME'] ?>
+				</span>
+			<? endif; ?>
+
 
 		</div>
 		<? if ($item['CHILDREN']): ?>
