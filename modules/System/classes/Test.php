1ad9e317731934404970a6795f8483024c7993f5
Reuniko Starwinged <319765+Reuniko@users.noreply.github.com>
2018-02-14 13:48:55 +0300

    tests

--- modules/System/classes/Test.php
+++ modules/System/classes/Test.php
@@ -18,6 +18,16 @@ class Test extends Instanceable {
 	 */
 	public $results = [];
 
+	public function __construct() {
+		$ReflectionClass = new \ReflectionClass(static::class);
+		$methods = $ReflectionClass->getMethods(\ReflectionMethod::IS_PUBLIC);
+		foreach ($methods as $method) {
+			if (substr($method->name, 0, 4) === 'Test') {
+				$this->tests[$method->name] = trim(substr($method->getDocComment(), 3, -2)) ?: $method->name;
+			}
+		}
+	}
+
 	public function Run($test) {
 		return (new \ReflectionMethod(static::class, $test))->invoke($this);
 	}
