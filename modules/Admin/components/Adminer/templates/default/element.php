0ddd5e19040c35211a703ae9e0c1276479196ebc
Reuniko Starwinged <319765+Reuniko@users.noreply.github.com>
2018-02-15 14:04:57 +0300

    Adminer element styles

--- modules/Admin/components/Adminer/templates/default/element.php
+++ modules/Admin/components/Adminer/templates/default/element.php
@@ -1,92 +1,111 @@
 <?php /** @var \Admin\Adminer $this */ ?>
 <?php $this->Debug($RESULT, '$RESULT'); ?>
 <?php $this->Debug($this->SCRUD->composition, 'composition'); ?>
+<?
+if ($RESULT['MODE'] === 'Create') {
+	$this->ENGINE->TITLE = "Добавление элемента '{$this->SCRUD->name}'";
+} else {
+	$this->ENGINE->TITLE = "Редактирование элемента №{$RESULT['DATA']['ID']} '{$this->SCRUD->name}'";
+}
+?>
 
-<h2>
-	<? if ($RESULT['MODE'] === 'Create'): ?>
-		Добавление элемента
-	<? else: ?>
-		Редактирование элемента №<?= $RESULT['DATA']['ID'] ?>
-	<? endif; ?>
-</h2>
+<div class="adminer">
 
-<form method="post" enctype="multipart/form-data" class="form-horizontal adminer">
+	<!-- Nav tabs -->
+	<ul class="nav nav-tabs" role="tablist">
+		<li class="nav-item">
+			<a class="nav-link active" data-toggle="tab" href="#element" role="tab">Элемент</a>
+		</li>
+	</ul>
 
-	<input type="hidden" name="ACTION" value="<?= $RESULT['MODE'] ?>"/>
+	<!-- Tab panes -->
+	<div class="tab-content">
+		<div class="tab-pane active tab-element" id="element" role="tabpanel">
 
-	<? @include($this->PathInclude('element_head.php')); ?>
+			<form method="post" enctype="multipart/form-data" class="form-horizontal">
 
-	<? foreach ($this->SCRUD->composition as $group_code => $group): ?>
-		<h3 class="group_header"><?= $group['NAME'] ?: "{{$group_code}}" ?></h3>
-		<? foreach ($group['FIELDS'] as $code => $field): ?>
-			<div class="form-group row">
-				<label
-					class="col-sm-3 col-form-label text-right"
-					for="<?= $code ?>"
-					title="<?= $code ?>"
-				>
-					<?= $field['NAME'] ?: "{{$code}}" ?>
-				</label>
-				<div class="col-sm-8">
-					<?
-					$value = $RESULT['DATA'][$code];
-					$inc = strtolower($field['VIEW']) ?: strtolower($field['TYPE']);
+				<input type="hidden" name="ACTION" value="<?= $RESULT['MODE'] ?>"/>
 
-					try {
-						require($this->Path('controls/' . $inc . '.php'));
-					} catch (\Exception $error) {
-						require($this->Path('controls/' . '_default' . '.php'));
-					}
-					?>
+				<? @include($this->PathInclude('element_head.php')); ?>
+
+				<? foreach ($this->SCRUD->composition as $group_code => $group): ?>
+					<h3 class="group_header"><?= $group['NAME'] ?: "{{$group_code}}" ?></h3>
+					<? foreach ($group['FIELDS'] as $code => $field): ?>
+						<div class="form-group row">
+							<label
+								class="col-sm-3 col-form-label text-right"
+								for="<?= $code ?>"
+								title="<?= $code ?>"
+							>
+								<?= $field['NAME'] ?: "{{$code}}" ?>
+							</label>
+							<div class="col-sm-8">
+								<?
+								$value = $RESULT['DATA'][$code];
+								$inc = strtolower($field['VIEW']) ?: strtolower($field['TYPE']);
+
+								try {
+									require($this->Path('controls/' . $inc . '.php'));
+								} catch (\Exception $error) {
+									require($this->Path('controls/' . '_default' . '.php'));
+								}
+								?>
+							</div>
+						</div>
+					<? endforeach; ?>
+				<? endforeach; ?>
+
+				<? @include($this->PathInclude('element_foot.php')); ?>
+
+				<br/>
+
+				<div class="buttons">
+
+
+					<button
+						class="btn btn-primary"
+						type="submit"
+						name="REDIRECT"
+						value="Return"
+					>
+						<i class="fa fa-save"></i>
+						Сохранить
+					</button>
+
+					<button
+						class="btn btn-success"
+						type="submit"
+						name="REDIRECT"
+						value="Stay"
+					>
+						<i class="fa fa-save"></i>
+						Применить
+					</button>
+
+					<a
+						class="btn btn-secondary"
+						href="?"
+					>
+						<i class="fa fa-ban"></i>
+						Вернуться
+					</a>
+
+					<button
+						class="btn btn-danger float-right"
+						type="submit"
+						name="ACTION"
+						value="Delete"
+					>
+						<i class="fa fa-eraser"></i>
+						Удалить
+					</button>
+
 				</div>
-			</div>
-		<? endforeach; ?>
-	<? endforeach; ?>
-
-	<? @include($this->PathInclude('element_foot.php')); ?>
-
-	<br/>
-
-	<div class="buttons">
+			</form>
 
 
-		<button
-			class="btn btn-primary"
-			type="submit"
-			name="REDIRECT"
-			value="Return"
-		>
-			<i class="fa fa-save"></i>
-			Сохранить
-		</button>
-
-		<button
-			class="btn btn-success"
-			type="submit"
-			name="REDIRECT"
-			value="Stay"
-		>
-			<i class="fa fa-save"></i>
-			Применить
-		</button>
-
-		<a
-			class="btn btn-secondary"
-			href="?"
-		>
-			<i class="fa fa-ban"></i>
-			Вернуться
-		</a>
-
-		<button
-			class="btn btn-danger pull-right"
-			type="submit"
-			name="ACTION"
-			value="Delete"
-		>
-			<i class="fa fa-eraser"></i>
-			Удалить
-		</button>
-
+		</div>
 	</div>
-</form>
+
+
+</div>
