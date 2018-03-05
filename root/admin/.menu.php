dfd5dd3ac13411c3eb0d041a6326298f9ee90765
Reuniko Starwinged <319765+Reuniko@users.noreply.github.com>
2018-03-05 15:18:31 +0300

    tracker init

--- root/admin/.menu.php
+++ root/admin/.menu.php
@@ -1,83 +1,101 @@
 <?php
-return [
-	'System'  => [
-		'NAME'     => 'Система',
-		'LINK'     => '/admin/System/',
-		'CHILDREN' => [
-			'panel'               => [
-				'NAME'     => 'Панель управления',
-				'LINK'     => '/admin/',
-				'CHILDREN' => [
-					'~upgrade' => [
-						'NAME' => '~upgrade',
-						'LINK' => '/admin/_upgrade.php',
+/** @var \System\Component $this */
+$menu = [];
+if ($this->ENGINE->modules['System'])
+	$menu += [
+		'System' => [
+			'NAME'     => 'Система',
+			'LINK'     => '/admin/System/',
+			'CHILDREN' => [
+				'panel'               => [
+					'NAME'     => 'Панель управления',
+					'LINK'     => '/admin/',
+					'CHILDREN' => [
+						'~upgrade' => [
+							'NAME' => '~upgrade',
+							'LINK' => '/admin/_upgrade.php',
+						],
 					],
 				],
-			],
-			'System_Modules'      => [
-				'NAME' => 'Модули',
-				'LINK' => '/admin/System/Modules.php',
-			],
-			'System_Content'      => [
-				'NAME' => 'Контент',
-				'LINK' => '/admin/System/Content.php',
-			],
-			'System_Users'        => [
-				'NAME' => 'Пользователи',
-				'LINK' => '/admin/System/Users.php',
-			],
-			'System_Groups'       => [
-				'NAME' => 'Группы',
-				'LINK' => '/admin/System/Groups.php',
-			],
-			'System_Users2Groups' => [
-				'NAME' => 'Пользователи в группах',
-				'LINK' => '/admin/System/Users2Groups.php',
-			],
-			'System_Files'        => [
-				'NAME' => 'Файлы',
-				'LINK' => '/admin/System/Files.php',
-			],
-			'Admin_TableSettings' => [
-				'NAME' => 'Настройки',
-				'LINK' => '/admin/Admin/TableSettings.php',
+				'System_Modules'      => [
+					'NAME' => 'Модули',
+					'LINK' => '/admin/System/Modules.php',
+				],
+				'System_Content'      => [
+					'NAME' => 'Контент',
+					'LINK' => '/admin/System/Content.php',
+				],
+				'System_Users'        => [
+					'NAME' => 'Пользователи',
+					'LINK' => '/admin/System/Users.php',
+				],
+				'System_Groups'       => [
+					'NAME' => 'Группы',
+					'LINK' => '/admin/System/Groups.php',
+				],
+				'System_Users2Groups' => [
+					'NAME' => 'Пользователи в группах',
+					'LINK' => '/admin/System/Users2Groups.php',
+				],
+				'System_Files'        => [
+					'NAME' => 'Файлы',
+					'LINK' => '/admin/System/Files.php',
+				],
 			],
 		],
-	],
-	'Testing' => [
-		'NAME'     => 'Тесты',
-		'LINK'     => '/admin/Testing',
-		'CHILDREN' => [
-			'Tables' => [
-				'NAME'     => 'Таблицы',
-				'CHILDREN' => [
-					'Table1' => [
-						'NAME' => 'Table1',
-						'LINK' => '/admin/Testing/Table1.php',
-					],
-					'Grades' => [
-						'NAME' => 'Grades',
-						'LINK' => '/admin/Testing/Grades.php',
-					],
-					'Rooms' => [
-						'NAME' => 'Rooms',
-						'LINK' => '/admin/Testing/Rooms.php',
-					],
-					'Students' => [
-						'NAME' => 'Students',
-						'LINK' => '/admin/Testing/Students.php',
-					],
-					'Timetable' => [
-						'NAME' => 'Timetable',
-						'LINK' => '/admin/Testing/Timetable.php',
-					],
-				],
-			],
+	];
 
-			'SCRUD' => [
-				'NAME' => 'Тестирование SCRUD',
-				'LINK' => '/admin/Testing/SCRUD.php',
+if ($this->ENGINE->modules['Admin'])
+	$menu += [
+		'Admin' => [
+			'NAME'     => 'Админка',
+			'LINK'     => '/admin/Admin/',
+			'CHILDREN' => [
+				'Admin_TableSettings' => [
+					'NAME' => 'Настройки',
+					'LINK' => '/admin/Admin/TableSettings.php',
+				],
 			],
 		],
-	],
-];
+	];
+
+if ($this->ENGINE->modules['Testing'])
+	$menu += [
+		'Testing' => [
+			'NAME'     => 'Тесты',
+			'LINK'     => '/admin/Testing',
+			'CHILDREN' => [
+				'Tables' => [
+					'NAME'     => 'Таблицы',
+					'CHILDREN' => [
+						'Table1'    => [
+							'NAME' => 'Table1',
+							'LINK' => '/admin/Testing/Table1.php',
+						],
+						'Grades'    => [
+							'NAME' => 'Grades',
+							'LINK' => '/admin/Testing/Grades.php',
+						],
+						'Rooms'     => [
+							'NAME' => 'Rooms',
+							'LINK' => '/admin/Testing/Rooms.php',
+						],
+						'Students'  => [
+							'NAME' => 'Students',
+							'LINK' => '/admin/Testing/Students.php',
+						],
+						'Timetable' => [
+							'NAME' => 'Timetable',
+							'LINK' => '/admin/Testing/Timetable.php',
+						],
+					],
+				],
+				'SCRUD'  => [
+					'NAME' => 'Тестирование SCRUD',
+					'LINK' => '/admin/Testing/SCRUD.php',
+				],
+			],
+		],
+	];
+
+return $menu;
