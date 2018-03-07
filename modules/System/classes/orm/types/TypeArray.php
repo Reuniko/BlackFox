2d9328293b0273e95ca86f971a5da626db294872
Reuniko Starwinged <319765+Reuniko@users.noreply.github.com>
2018-03-07 11:51:00 +0300

    UnitTasks

---
+++ modules/System/classes/orm/types/TypeArray.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace System;
+
+class TypeArray extends TypeText {
+	public static $name = 'Array';
+	public static $code = 'ARRAY';
+
+	public function FormatInputValue($value) {
+		$value = is_array($value) ? $value : [$value];
+		$value = json_encode($value, JSON_UNESCAPED_UNICODE);
+		return parent::FormatInputValue($value);
+	}
+
+	public function FormatOutputValue($element) {
+		$code = $this->info['CODE'];
+		$element[$code] = json_decode($element[$code], true);
+		if (json_last_error()) {
+			$element[$code] = [];
+		}
+		return $element;
+	}
+
+}
