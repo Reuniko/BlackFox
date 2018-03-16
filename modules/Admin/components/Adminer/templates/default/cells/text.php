7a7815de9e13b95446e33e7d14b29e8ea97adeee
Reuniko <reuniko@gmail.com>
2018-03-16 10:57:56 +0300

    ?????? ?9 - ????????? ???????? ????? ???????? ?????? ? ????? ??????

--- modules/Admin/components/Adminer/templates/default/cells/text.php
+++ modules/Admin/components/Adminer/templates/default/cells/text.php
@@ -1,8 +1,12 @@
-<? /** @var string $value */ ?>
 <?
-$value = strip_tags($value);
-if (mb_strlen($value) > 100) {
-	$value = mb_substr($value, 0, 100) . '...';
+/** @var string|array $value */
+/** @var \Admin\Adminer $this */
+/** @var string $code */
+if ($this->SCRUD->structure[$code]['WYSIWYG']) {
+	$value = htmlspecialchars_decode($value);
+	$value = strip_tags($value);
+}
+if (mb_strlen($value) > 250) {
+	$value = mb_substr($value, 0, 250) . '...';
 }
 echo $value;
-?>
