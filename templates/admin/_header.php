8a858b64f2f0bf27d9179bc50b8ae7f543835c1b
Reuniko <reuniko@gmail.com>
2018-05-18 14:48:07 +0300

    update admin template: bootstrap 4.0.0, font-awesome

--- templates/admin/_header.php
+++ templates/admin/_header.php
@@ -14,11 +14,10 @@
 <script src="<?= $this->TEMPLATE_PATH ?>/js/jquery.min.js"></script>
 <script src="<?= $this->TEMPLATE_PATH ?>/js/jquery-ui.min.js"></script>
 
-<link rel="stylesheet" href="<?= $this->TEMPLATE_PATH ?>/bootstrap/css/bootstrap.min.css">
-<script src="<?= $this->TEMPLATE_PATH ?>/bootstrap/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="<?= $this->TEMPLATE_PATH ?>/lib/bootstrap/css/bootstrap.min.css">
+<script src="<?= $this->TEMPLATE_PATH ?>/lib/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-<link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
-
+<link href="<?= $this->TEMPLATE_PATH ?>/lib/fontawesome/css/fontawesome-all.min.css" rel="stylesheet">
 
 <script src="<?= $this->TEMPLATE_PATH ?>/lib/ckeditor/ckeditor.js"></script>
 
