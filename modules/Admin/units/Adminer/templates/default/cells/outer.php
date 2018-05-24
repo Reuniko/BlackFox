8c2754def1ade102f8f5a840df37c5acd79783a9
Reuniko Starwinged <319765+Reuniko@users.noreply.github.com>
2018-05-24 17:24:10 +0300

    Adminer improvements

--- modules/Admin/units/Adminer/templates/default/cells/outer.php
+++ modules/Admin/units/Adminer/templates/default/cells/outer.php
@@ -5,14 +5,10 @@
 /** @var \System\SCRUD $Link */
 $Link = $this->SCRUD->structure[$code]['LINK']::I();
 $url = $Link->GetAdminUrl();
-$ID = $RESULT['DATA'][$code]['ID'];
 ?>
 <? if (!is_array($value)): ?>
-	<nobr>[<a href="<?= $url ?>?ID=<?= $value['ID'] ?>"><?= $value['ID'] ?></a>]</nobr>
+	<nobr>[<a href="<?= $url ?>?ID=<?= $value ?>"><?= $value ?></a>]</nobr>
 <? elseif (!empty($value['ID'])): ?>
 	<nobr>[<a href="<?= $url ?>?ID=<?= $value['ID'] ?>"><?= $value['ID'] ?></a>]</nobr>
-	<? unset($value['ID']) ?>
-	<? foreach ($value as $code => $element): ?>
-		<?= $element ?>
-	<? endforeach; ?>
+	<?= $Link->GetElementTitle($value); ?>
 <? endif; ?>
