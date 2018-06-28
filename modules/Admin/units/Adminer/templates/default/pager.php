b9cc0ff98ca476928b015737e204ca145d5b081b
Reuniko Starwinged <319765+Reuniko@users.noreply.github.com>
2018-06-28 15:16:39 +0300

    ?182 ???????? ?????????? ????? ???????? ? ??????

--- modules/Admin/units/Adminer/templates/default/pager.php
+++ modules/Admin/units/Adminer/templates/default/pager.php
@@ -9,38 +9,41 @@ $get = $_GET;
 unset($get[$RESULT['VARIABLE']]);
 $base = http_build_query($get);
 ?>
+<div class="pager">
 
-<div>
-	<span>Всего <strong><?= $RESULT['DATA']['PAGER']['TOTAL'] ?></strong> элементов.</span>
-	<span>Отображено <strong><?= $RESULT['DATA']['PAGER']['SELECTED'] ?></strong> элементов.</span>
+	<div class="alert alert-info float-right p-2">
+		<span>Отображено <strong><?= $RESULT['DATA']['PAGER']['SELECTED'] ?></strong> элементов.</span>
+		<span>Всего <strong><?= $RESULT['DATA']['PAGER']['TOTAL'] ?></strong> элементов.</span>
+	</div>
+
+	<nav>
+		<ul class="pagination">
+			<? foreach ($RESULT['PAGES'] as $page): ?>
+				<? if ($page['ACTIVE']): ?>
+					<li class="page-item active">
+						<a class="page-link">
+							<?= $page["INDEX"] ?>
+						</a>
+					</li>
+				<? elseif ($page['...']): ?>
+					<li class="page-item">
+						<a
+							class="page-link"
+							href="javascript:if(page = prompt('Введите номер страницы', '')){window.location='?<?= $base ?>&<?= $RESULT['VARIABLE'] ?>='+page}">
+							<?= $page["INDEX"] ?>
+						</a>
+					</li>
+				<? else : ?>
+					<li class="page-item">
+						<a
+							class="page-link"
+							href="?<?= http_build_query(array_merge($_GET, array($RESULT['VARIABLE'] => $page['INDEX']))) ?>">
+							<?= $page["INDEX"] ?>
+						</a>
+					</li>
+				<? endif; ?>
+			<? endforeach; ?>
+		</ul>
+	</nav>
+
 </div>
-
-<nav>
-	<ul class="pagination">
-	<? foreach ($RESULT['PAGES'] as $page): ?>
-		<? if ($page['ACTIVE']): ?>
-			<li class="page-item active">
-				<a class="page-link">
-					<?= $page["INDEX"] ?>
-				</a>
-			</li>
-		<? elseif ($page['...']): ?>
-			<li class="page-item">
-				<a
-					class="page-link"
-					href="javascript:if(page = prompt('Введите номер страницы', '')){window.location='?<?= $base ?>&<?= $RESULT['VARIABLE'] ?>='+page}">
-					<?= $page["INDEX"] ?>
-				</a>
-			</li>
-		<? else : ?>
-			<li class="page-item">
-				<a
-					class="page-link"
-					href="?<?= http_build_query(array_merge($_GET, array($RESULT['VARIABLE'] => $page['INDEX']))) ?>">
-					<?= $page["INDEX"] ?>
-				</a>
-			</li>
-		<? endif; ?>
-	<? endforeach; ?>
-	</ul>
-</nav>
