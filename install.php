ed8fb9bef91c72d29135c91c692844c6c35abc55
Reuniko Starwinged <319765+Reuniko@users.noreply.github.com>
2018-07-27 12:05:29 +0300

    Script for initial install from console

---
+++ install.php
@@ -0,0 +1,32 @@
+<?php
+// Script for initial install from console
+
+if (php_sapi_name() <> 'cli') die('Console usage only');
+$_SERVER["DOCUMENT_ROOT"] = __DIR__ . '/../';
+
+require_once("modules/system/classes/patterns/Instanceable.php");
+require_once("modules/system/classes/Engine.php");
+require_once("modules/system/debug.php");
+
+\System\Engine::I()->RegisterModuleClasses('System');
+\System\Module::I()->Upgrade();
+\System\Modules::I()->Delete(['ID' => 'System']);
+\System\Modules::I()->Create(['ID' => 'System']);
+
+\System\Engine::I()->RegisterModuleClasses('Admin');
+\Admin\Module::I()->Upgrade();
+\System\Modules::I()->Delete(['ID' => 'Admin']);
+\System\Modules::I()->Create(['ID' => 'Admin']);
+
+$group_id = \System\Groups::I()->Create([
+	'CODE' => 'root',
+	'NAME' => 'Root',
+]);
+$user_id = \System\Users::I()->Create([
+	'LOGIN'    => 'Root',
+	'PASSWORD' => '123456',
+]);
+\System\Users2Groups::I()->Create([
+	'USER'  => $user_id,
+	'GROUP' => $group_id,
+]);
