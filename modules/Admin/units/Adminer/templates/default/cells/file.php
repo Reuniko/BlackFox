88f24acc0415b0d10bd27dbeca8f0fce216a609d
Reuniko <reuniko@gmail.com>
2018-12-28 13:08:06 +0300

    micro

--- modules/Admin/units/Adminer/templates/default/cells/file.php
+++ modules/Admin/units/Adminer/templates/default/cells/file.php
@@ -1,5 +1,7 @@
 <? /** @var array $value */ ?>
 <? debug($value) ?>
-[<a target="_blank" href="/admin/System/Files.php?ID=<?= $value['ID'] ?>"><?= $value['ID'] ?></a>]
-<a target="_blank" href="<?= $value['SRC'] ?>"><?= $value['NAME'] ?></a>
+<? if (!empty($value)): ?>
+	[<a target="_blank" href="/admin/System/Files.php?ID=<?= $value['ID'] ?>"><?= $value['ID'] ?></a>]
+	<a target="_blank" href="<?= $value['SRC'] ?>"><?= $value['NAME'] ?></a>
+<? endif; ?>
 
