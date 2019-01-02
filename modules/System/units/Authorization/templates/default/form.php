1e033374a014ec85aa58673c72ea88b2c4c4e54e
Reuniko <reuniko@gmail.com>
2019-01-02 07:56:27 +0300

    unit Auth update

--- modules/System/units/Authorization/templates/default/form.php
+++ modules/System/units/Authorization/templates/default/form.php
@@ -1,16 +1,15 @@
 <?php /** @var \System\Unit $this */ ?>
 <?php $this->Debug($RESULT, '$RESULT'); ?>
+
 <div class="row">
 	<div
-		style="margin-top: 200px;"
 		class="
-			well
-			col-sm-8 col-sm-offset-2
-			col-md-6 col-md-offset-3
+			col-sm-8 offset-sm-2
+			col-md-6 offset-md-3
 			"
 	>
-		<form class="form" method="POST">
-			<? $this->ShowAlerts(); ?>
+		<? $this->ShowAlerts(); ?>
+		<form class="form card p-3" method="POST">
 			<h2><?= $this->PARAMS['TITLE'] ?: 'Вход в систему' ?></h2>
 			<div class="form-group">
 				<input type="text" name="login" value="<?= $RESULT['LOGIN'] ?>" class="form-control" placeholder="Логин"/>
