3af9d47d6b22de9fd8a2f435634d9ea797fe4557
Reuniko Starwinged <319765+Reuniko@users.noreply.github.com>
2019-05-13 10:08:03 +0300

    micro

--- modules/System/T.php
+++ modules/System/T.php
@@ -1,12 +1,13 @@
 <?php
 if (!function_exists('T')) {
 	/**
-	 * Picks
+	 * It finds language code from $_SESSION['USER']['LANG']
+	 * It picks corresponding value from input array
 	 *
 	 * @param array $variants
 	 * @return string
 	 */
-	function T($variants) {
+	function T(array $variants) {
 		return $variants[$_SESSION['USER']['LANG']] ?: reset($variants);
 	}
 }
