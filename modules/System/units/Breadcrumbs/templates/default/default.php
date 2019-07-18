b3631713ab6173fe4d45825ae90e97b22da28b9a
Reuniko <reuniko@gmail.com>
2019-07-18 09:56:35 +0300

    wip

--- modules/System/units/Breadcrumbs/templates/default/default.php
+++ modules/System/units/Breadcrumbs/templates/default/default.php
@@ -2,7 +2,7 @@
 <?php /** @var array $RESULT */ ?>
 
 <? if (!empty($RESULT['BREADCRUMBS'])): ?>
-	<ol class="breadcrumb">
+	<ol class="system breadcrumb">
 		<? foreach ($RESULT['BREADCRUMBS'] as $breadcrumb): ?>
 			<li class="breadcrumb-item">
 				<a href="<?= $breadcrumb['LINK'] ?>"><?= $breadcrumb['NAME'] ?></a>
